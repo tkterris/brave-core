a94cef504c99b39f3a1ab2ca55575f35a2432575
Mario Sanchez Prada <mario@igalia.com>
2021-04-15 17:38:33 +0200

    Pass newly-added parameter to Profile::GetPrimaryOTRProfile()
    We need to pass true to preserve the previous behaviour now.
    
    Chromium change:
    
    https://source.chromium.org/chromium/chromium/src/+/94e457276df130575423e518181ece753734d6b9
    
    commit 94e457276df130575423e518181ece753734d6b9
    Author: Ramin Halavati <rhalavati@chromium.org>
    Date:   Tue Apr 13 12:27:51 2021 +0000
    
        Remove default value of Profile::GetOffTheRecordProfile()
    
        Removes the default value of the newly added |create_if_needed|
        aregument of Profile::GetOffTheRecordProfile() and updates all use cases
        with adding the new argument.
    
        This CL does not change any behavior.
    
        AX-Relnotes: n/a.
        Bug: 1191315

--- browser/search_engines/search_engine_provider_service_browsertest.cc
+++ browser/search_engines/search_engine_provider_service_browsertest.cc
@@ -39,7 +39,8 @@ TemplateURLData CreateTestSearchEngine() {
 IN_PROC_BROWSER_TEST_F(SearchEngineProviderServiceTest,
                        PrivateWindowPrefTestWithNonQwantRegion) {
   Profile* profile = browser()->profile();
-  Profile* incognito_profile = profile->GetPrimaryOTRProfile();
+  Profile* incognito_profile =
+      profile->GetPrimaryOTRProfile(/*create_if_needed=*/true);
 
   // This test case is only for non-qwant region.
   if (brave::IsRegionForQwant(profile))
@@ -88,7 +89,8 @@ IN_PROC_BROWSER_TEST_F(SearchEngineProviderServiceTest,
 IN_PROC_BROWSER_TEST_F(SearchEngineProviderServiceTest,
                        PrivateWindowTestWithQwantRegion) {
   Profile* profile = browser()->profile();
-  Profile* incognito_profile = profile->GetPrimaryOTRProfile();
+  Profile* incognito_profile =
+      profile->GetPrimaryOTRProfile(/*create_if_needed=*/true);
 
   // This test case is only for qwant region.
   if (!brave::IsRegionForQwant(profile))
