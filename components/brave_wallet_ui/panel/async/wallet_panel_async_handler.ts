69c7004e1893862c3dae7d6accd87e36bb9ddb8a
Brian R. Bondy <netzen@gmail.com>
2021-04-29 15:56:24 -0400

    Use a diff dir for the Wallet panel and page

--- components/brave_wallet_ui/panel/async/wallet_panel_async_handler.ts
+++ components/brave_wallet_ui/panel/async/wallet_panel_async_handler.ts
@@ -4,9 +4,9 @@
 // you can obtain one at http://mozilla.org/MPL/2.0/.
 
 import { MiddlewareAPI, Dispatch, AnyAction } from 'redux'
-import AsyncActionHandler from '../../common/AsyncActionHandler'
+import AsyncActionHandler from '../../../common/AsyncActionHandler'
 import * as Actions from '../actions/wallet_panel_actions'
-import { State, WalletPanelReducerState } from '../constants/types'
+import { State, WalletPanelReducerState } from '../../constants/types'
 import { AccountPayloadType } from '../constants/action_types'
 
 const handler = new AsyncActionHandler()
